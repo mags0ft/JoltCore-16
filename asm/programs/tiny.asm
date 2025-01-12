7908c0298ed6fda2a4e0c055674616f2f376535c
mags0ft <magnusschlinsog@gmail.com>
2025-01-12 18:07:05 +0100

    improve test program

--- asm/programs/tiny.asm
+++ asm/programs/tiny.asm
@@ -3,10 +3,13 @@
 
 main:
     add r2, r2, #5                 ; starting off with a standard addition...
+    add r2, #5, r2                 ; another one to make sure we didn't break immediates
 
     add r0, r0, (lshift r0, #2)    ; an add instruction with encapsulated operation
     add r1, (rshift r1, #4), r1    ; another one, just to make sure everything works
     
+    sub r2, (not r3), r4           ; test whether "not" works
+
     halt                           ; stop the program execution
 
 ; build in all possible formats using:
