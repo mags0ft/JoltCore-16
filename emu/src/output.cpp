3359bcb89919d7006ed0cbdce0c5e924fa9c807f
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-06-26 20:53:42 +0200

    add explanation to JCEMU output file

--- emu/src/output.cpp
+++ emu/src/output.cpp
@@ -7,6 +7,7 @@ namespace Output
 
     void print_info(std::string info, bool omit_info = false)
     {
+        // Method for handling formatted printing
         std::cout << (omit_info ? "" : "jcemu: ") << info << std::endl;
     }
 
